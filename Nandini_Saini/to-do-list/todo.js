6c2d76a9eef95f6c43eecb5969799a945c1604da
Nandini Saini <71938658+nandini-412@users.noreply.github.com>
2020-10-28 15:31:35 +0530

    Update todo.js

--- Nandini_Saini/to-do-list/todo.js
+++ Nandini_Saini/to-do-list/todo.js
@@ -1,20 +1,26 @@
-$("li").on("click" ,"li", function(){
-	$(this).toggleClass("completed");
 
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
 });
-$("ul").on("click","span", function(event){
+
+
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
+
 $("input[type='text']").keypress(function(event){
-	if(event.which===13){
-		var todoText=$(this).val();
+	if(event.which === 13){
+		
+		var todoText = $(this).val();
 		$(this).val("");
-		$("ul").append("<li><span><i class='fas fa-trash-alt'></i> </span>"+ todoText +"</li>");
+		
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
 	}
 });
+
 $(".fa-plus").click(function(){
 	$("input[type='text']").fadeToggle();
 });
